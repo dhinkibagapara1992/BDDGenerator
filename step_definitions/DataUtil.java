3f1a0b39ec21e3502c2618e4f6e53e3f7555fbae
dhinkibagapara1992 <dhinkibagapara@gmail.com>
2025-05-22 15:57:24 +0530

    Update DataUtil.java

--- step_definitions/DataUtil.java
+++ step_definitions/DataUtil.java
@@ -1,7 +1,16 @@
 package utils;
+import java.util.Map;
+import java.util.HashMap;
+
 public class DataUtil {
+    private static Map<String, String> data = new HashMap<>();
+
+    static {
+        // Populate from JSON or config
+        data.put("login_input", "DhinkiBagaPara@123");
+    }
+
     public static String getValue(String key) {
-        // Implement lookup
-        return "";
+        return data.getOrDefault(key, "");
     }
 }
