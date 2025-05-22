f0208e255439f40f40258966d9821a532982aede
dhinkibagapara1992 <dhinkibagapara@gmail.com>
2025-05-22 16:18:23 +0530

    Update JsExecutorUtil.java

--- java-bdd/src/main/java/com/dhinki/bddgenerator/utils/JsExecutorUtil.java
+++ java-bdd/src/main/java/com/dhinki/bddgenerator/utils/JsExecutorUtil.java
@@ -1,1 +1,12 @@
+package com.dhinki.bddgenerator.utils;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class JsExecutorUtil {
+    public static void waitForPageLoad(WebDriver driver) {
+        new WebDriverWait(driver, java.time.Duration.ofSeconds(30)).until(
+            webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+    }
+}
