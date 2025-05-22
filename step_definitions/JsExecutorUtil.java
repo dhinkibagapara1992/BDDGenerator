139404bdcc7dbc45e5674162ba7177c3f5b2f720
dhinkibagapara1992 <dhinkibagapara@gmail.com>
2025-05-22 15:53:28 +0530

    Update JsExecutorUtil.java

--- step_definitions/JsExecutorUtil.java
+++ step_definitions/JsExecutorUtil.java
@@ -1,1 +1,9 @@
-
+package utils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+public class JsExecutorUtil {
+    public static void waitForPageLoad(WebDriver driver) {
+        new org.openqa.selenium.support.ui.WebDriverWait(driver, 30).until(
+            webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+    }
+}
