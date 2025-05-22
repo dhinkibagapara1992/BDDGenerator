531683e9b4b74fa94f104abf434c2eea75baba81
dhinkibagapara1992 <dhinkibagapara@gmail.com>
2025-05-22 15:52:31 +0530

    Update StepDefinitions.java

--- step_definitions/StepDefinitions.java
+++ step_definitions/StepDefinitions.java
@@ -1,1 +1,17 @@
+package step_definitions;
 
+import io.cucumber.java.en.When;
+import utils.ObjectRepositoryUtil;
+import utils.DataUtil;
+import utils.JsExecutorUtil;
+
+public class StepDefinitions {
+    @When("^I perform the action \"([^\"]*)\" on element \"([^\"]*)\" with value \"([^\"]*)\"$")
+    public void performActionWithValue(String action, String element, String value) throws Exception {
+        // Implement action using ObjectRepositoryUtil and DataUtil
+    }
+    @When("^I perform the action \"([^\"]*)\" on element \"([^\"]*)\"$")
+    public void performAction(String action, String element) throws Exception {
+        // Implement action using ObjectRepositoryUtil
+    }
+}
