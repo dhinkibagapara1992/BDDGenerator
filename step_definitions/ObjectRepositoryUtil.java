21eb7ec904dd409824de8026ea570c88348401d3
dhinkibagapara1992 <dhinkibagapara@gmail.com>
2025-05-22 15:57:03 +0530

    Update ObjectRepositoryUtil.java

--- step_definitions/ObjectRepositoryUtil.java
+++ step_definitions/ObjectRepositoryUtil.java
@@ -1,8 +1,22 @@
 package utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import java.util.Map;
+import java.util.HashMap;
+
 public class ObjectRepositoryUtil {
-    public static Map<String, String> getLocator(String elementName) {
-        // Implement lookup
-        return null;
+    private static Map<String, String> repo = new HashMap<>();
+
+    static {
+        // Populate from JSON or config
+        repo.put("login_input", "//*[@id='login']");
+        repo.put("submit_button", "//*[@id='submit']");
+    }
+
+    public static WebElement findElement(WebDriver driver, String elementName) {
+        String xpath = repo.get(elementName);
+        if (xpath == null) throw new RuntimeException("Element not found in repo");
+        return driver.findElement(By.xpath(xpath));
     }
 }
