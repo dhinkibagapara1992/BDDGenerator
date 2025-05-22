8fb7d7fefb68a8ab7f6f668b829eadf528bc979c
dhinkibagapara1992 <dhinkibagapara@gmail.com>
2025-05-22 15:55:58 +0530

    Update StepDefinitions.java

--- step_definitions/StepDefinitions.java
+++ step_definitions/StepDefinitions.java
@@ -4,14 +4,30 @@ import io.cucumber.java.en.When;
 import utils.ObjectRepositoryUtil;
 import utils.DataUtil;
 import utils.JsExecutorUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class StepDefinitions {
+
+    WebDriver driver; // Assume driver is initialized elsewhere
+
     @When("^I perform the action \"([^\"]*)\" on element \"([^\"]*)\" with value \"([^\"]*)\"$")
     public void performActionWithValue(String action, String element, String value) throws Exception {
-        // Implement action using ObjectRepositoryUtil and DataUtil
+        WebElement el = ObjectRepositoryUtil.findElement(driver, element);
+        switch (action.toLowerCase()) {
+            case "entered":
+                el.clear();
+                el.sendKeys(DataUtil.getValue(value));
+                break;
+            case "clicked":
+                el.click();
+                break;
+            // Add more action handlers as needed
+        }
     }
+
     @When("^I perform the action \"([^\"]*)\" on element \"([^\"]*)\"$")
     public void performAction(String action, String element) throws Exception {
-        // Implement action using ObjectRepositoryUtil
+        performActionWithValue(action, element, "");
     }
 }
