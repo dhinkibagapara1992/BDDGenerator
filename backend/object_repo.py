a5dd65de6c55dfe34132980d78d879413bcfa9f0
dhinkibagapara1992 <dhinkibagapara@gmail.com>
2025-05-22 15:49:24 +0530

    Update object_repo.py

--- backend/object_repo.py
+++ backend/object_repo.py
@@ -1,1 +1,12 @@
+from ai_xpath import get_ai_xpath_suggestions
 
+class ObjectRepositoryManager:
+    def __init__(self, repo=None):
+        self.repo = repo if repo is not None else []
+
+    def add_object(self, obj):
+        obj['suggested_locators'] = get_ai_xpath_suggestions(obj)
+        self.repo.append(obj)
+
+    def to_json(self):
+        return self.repo
