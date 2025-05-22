29dfa3e401d18b6af7c6c83c45ac79ec60069bdf
dhinkibagapara1992 <dhinkibagapara@gmail.com>
2025-05-22 15:52:49 +0530

    Update ObjectRepositoryUtil.java

--- step_definitions/ObjectRepositoryUtil.java
+++ step_definitions/ObjectRepositoryUtil.java
@@ -1,1 +1,8 @@
-
+package utils;
+import java.util.Map;
+public class ObjectRepositoryUtil {
+    public static Map<String, String> getLocator(String elementName) {
+        // Implement lookup
+        return null;
+    }
+}
